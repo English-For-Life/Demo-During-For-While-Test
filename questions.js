a99a45e34cc690e8c4f756520aa073b5587339c2
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-03 18:38:01 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,30 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Nobody came to see me ..... I was in hospital.",
+        options:["for", "during", "while"],
+        answer: 2
+    },
+    {
+        q:"Can you hold my bag ..... I try on this jacket?",
+        options:["for", "during", "while"],
+        answer: 2
+    },
+    {
+        q:"When I fly anywhere, I never eat anything ..... the flight.",
+           options:["during", "while"],
+           answer: 0   
+       },
+       {
+        q:"The traffic was bad. We were stuck in a traffic jam ..... two hours.",
+        options:["for", "during"],
+        answer: 0
+    },
+    {
+        q:"I don’t have much free time ..... the week, but I relax at weekends.",
+        options:["for", "during"],
+        answer: 1
+    }
+
+
+]
